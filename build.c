0e1f08487f3658bae0375995f0210d82b0d20457
jimmyolo <victor0801x@gmail.com>
2026-09-17 00:43:49 +0800

    Build simdutf variant on all platforms from build.yml
    build_simdutf() fetches and compiles simdutf when CXXFLAGS asks for UWS_USE_SIMDUTF, in both the Unix and Windows build paths. build.yml keeps its push trigger and builds, smoke-tests and uploads the simdutf variant after the existing build.

--- build.c
+++ build.c
@@ -127,18 +127,22 @@ void build_boringssl(const char *arch) {
 
 }
 
+/* Returns CXXFLAGS; when it asks for UWS_USE_SIMDUTF, also builds simdutf.o, which the link picks up through *.o */
+char *build_simdutf(char *cpp_compiler, char *flags) {
+    char *cxxflags = getenv("CXXFLAGS") ? getenv("CXXFLAGS") : "";
+    if (strstr(cxxflags, "UWS_USE_SIMDUTF")) {
+        run("curl --create-dirs -fL -o simdutf/simdutf.h https://github.com/simdutf/simdutf/releases/download/v9.2.0/simdutf.h && curl --create-dirs -fL -o simdutf/simdutf.cpp https://github.com/simdutf/simdutf/releases/download/v9.2.0/simdutf.cpp");
+        run("%s %s -c -std=c++20 simdutf/simdutf.cpp", cpp_compiler, flags);
+    }
+    return cxxflags;
+}
+
 /* Build for Unix systems */
 void build(char *compiler, char *cpp_compiler, char *cpp_linker, char *os, const char *arch) {
 
     char *c_shared = "-DWIN32_LEAN_AND_MEAN -DLIBUS_USE_LIBUV -DLIBUS_USE_QUIC -I uWebSockets/uSockets/lsquic/include -I uWebSockets/uSockets/boringssl/include -pthread -DLIBUS_USE_OPENSSL" OPT_FLAGS " -c -fPIC -I uWebSockets/uSockets/src uWebSockets/uSockets/src/*.c uWebSockets/uSockets/src/eventing/*.c uWebSockets/uSockets/src/crypto/*.c";
     char *cpp_shared = "-DWIN32_LEAN_AND_MEAN -DUWS_WITH_PROXY -DUWS_REMOTE_ADDRESS_USERSPACE -DLIBUS_USE_LIBUV -DLIBUS_USE_QUIC -I uWebSockets/uSockets/boringssl/include -pthread -DLIBUS_USE_OPENSSL" OPT_FLAGS " -c -fPIC -std=c++20 -I uWebSockets/uSockets/src -I uWebSockets/src src/addon.cpp uWebSockets/uSockets/src/crypto/sni_tree.cpp";
-    char *cxxflags = getenv("CXXFLAGS") ? getenv("CXXFLAGS") : "";
-
-    /* UWS_USE_SIMDUTF needs simdutf; simdutf.o is linked through *.o */
-    if (strstr(cxxflags, "UWS_USE_SIMDUTF")) {
-        run("mkdir -p simdutf && curl -fL -o simdutf/simdutf.h https://github.com/simdutf/simdutf/releases/download/v9.2.0/simdutf.h && curl -fL -o simdutf/simdutf.cpp https://github.com/simdutf/simdutf/releases/download/v9.2.0/simdutf.cpp");
-        run("%s" OPT_FLAGS " -c -fPIC -std=c++20 simdutf/simdutf.cpp", cpp_compiler);
-    }
+    char *cxxflags = build_simdutf(cpp_compiler, OPT_FLAGS " -fPIC");
 
     for (unsigned int i = 0; i < sizeof(versions) / sizeof(struct node_version); i++) {
         run("%s %s -I targets/node-%s/include/node", compiler, c_shared, versions[i].name);
@@ -160,10 +164,11 @@ void build_windows(char *compiler, char *cpp_compiler, char *cpp_linker, char *o
 
     char *c_shared = "-DWIN32_LEAN_AND_MEAN -DLIBUS_USE_LIBUV -DLIBUS_USE_QUIC -IuWebSockets/uSockets/lsquic/include -IuWebSockets/uSockets/lsquic/wincompat -IuWebSockets/uSockets/boringssl/include -DLIBUS_USE_OPENSSL -O3 -c -IuWebSockets/uSockets/src uWebSockets/uSockets/src/*.c uWebSockets/uSockets/src/eventing/*.c uWebSockets/uSockets/src/crypto/*.c";
     char *cpp_shared = "-DWIN32_LEAN_AND_MEAN -DUWS_WITH_PROXY -DUWS_REMOTE_ADDRESS_USERSPACE -DLIBUS_USE_LIBUV -DLIBUS_USE_QUIC -IuWebSockets/uSockets/lsquic/include -IuWebSockets/uSockets/lsquic/wincompat -IuWebSockets/uSockets/boringssl/include -DLIBUS_USE_OPENSSL -O3 -c -std=c++20 -IuWebSockets/uSockets/src -IuWebSockets/src src/addon.cpp uWebSockets/uSockets/src/crypto/sni_tree.cpp";
+    char *cxxflags = build_simdutf(cpp_compiler, "-O3");
 
     for (unsigned int i = 0; i < sizeof(versions) / sizeof(struct node_version); i++) {
         run("%s %s -Itargets/node-%s/include/node", compiler, c_shared, versions[i].name);
-        run("%s %s -Itargets/node-%s/include/node", cpp_compiler, cpp_shared, versions[i].name);
+        run("%s %s %s -Isimdutf -Itargets/node-%s/include/node", cpp_compiler, cpp_shared, cxxflags, versions[i].name);
         run("%s -O3 *.o uWebSockets/uSockets/boringssl/%s/ssl.lib uWebSockets/uSockets/boringssl/%s/crypto.lib uWebSockets/uSockets/lsquic/src/liblsquic/Debug/lsquic.lib targets/node-%s/node.lib -ladvapi32 -std=c++20 -shared -o dist/uws_win32_%s_%s.node", cpp_compiler, arch, arch, versions[i].name, arch, versions[i].abi);
     }
 }
